da1097b9e16261da0ee4dacf07fe0d1104deacf8
MIZAEL MENDES DA SILVA <a95541100@rn.senac.local>
2025-03-11 17:25:18 -0300

    Add. Atividade.estrutura

---
+++ Aula_11_11.03.25/revisao/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./components/Button/Button";
+
+function App () {
+  return (
+  
+  <div>
+   <h1> Olá, mundo!  ❤️❤️</h1>
+   <p> Este é meu primeiro componente React </p>
+   <Button />
+  </div>   
+
+  );
+
+}
+
+ export default App;
